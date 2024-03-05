889a744b99cc96a97e977fe319f2760236887260
Arslan Ondar <Arslanondar2003@gmail.com>
2024-03-05 15:02:33 +0700

    menu-sidebar-commit

--- src/shared/layouts/sidebar/ui/index.tsx
+++ src/shared/layouts/sidebar/ui/index.tsx
@@ -1,4 +1,3 @@
-// import './App.css';
 import { FC, useEffect, useState } from 'react';
 import "../../../../app/styles/sidebar.css";
 import { Menu } from 'antd';
@@ -8,8 +7,8 @@ import { CalendarFilled, LogoutOutlined, UserOutlined } from "@ant-design/icons"
 
 const Sidebar: FC = () => {
   return (
- 
-        <div style={{width: "100%"}}>
+
+        <div className='sidebar_class'>
             <SideMenu />
             </div>
       )
@@ -17,28 +16,23 @@ const Sidebar: FC = () => {
 };
 
 
-function SideMenu<FC>(){
-  const [selected, setSelected] = useState('/booking')
+function SideMenu <FC> (){
+  const [state, setState] = useState('/booking')
   const navigate = useNavigate();
 
   useEffect(() => {
-    setSelected(`/${window.location.pathname.split("/")[2]}`)
+    setState(`/${window.location.pathname.split("/")[2]}`)
   },[])
 
   return (
     
-    <Menu 
-    
-      style={{
-        padding: "20px 20px", 
-        // background: "#2c94f5"
-      }}
+    <Menu className='menu_sidebar'
       theme='dark'
       selectedKeys={[`/${window.location.pathname.split("/")[2]}`]}
-      defaultSelectedKeys={[selected]}
+      defaultSelectedKeys={[state]}
       onClick={({ key }) => {
         
-          setSelected(key)
+          setState(key)
             navigate(`/${key}`)
         
       }}
@@ -82,120 +76,3 @@ function SideMenu<FC>(){
 
 
 export default Sidebar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { FC, useState } from 'react';
-// import { CalendarFilled, LogoutOutlined, UserOutlined } from "@ant-design/icons";
-// import type { MenuProps } from 'antd';
-// import { Menu } from 'antd';
-// import { useNavigate } from 'react-router-dom';
-
-
-// type MenuItem = Required<MenuProps>['items'][number];
-
-// function getItem(
-//     label: string,
-//     key: string,
-//     icon: React.ReactNode,
-// ): MenuItem {
-//     return {
-//         key,
-//         icon,
-//         label,
-//     } as MenuItem;
-// }
-
-// const items:  MenuItem[] = [
-//     getItem('Бронь', 'booking', <CalendarFilled />),
-//     getItem('Номера', 'room', <CalendarFilled />),
-//     getItem('Отель', 'hotel', <CalendarFilled />),
-//     getItem('Аккаунт', 'account', <UserOutlined />),
-//     { type: "divider" },
-//     getItem('Выйти', 'logout', <LogoutOutlined />),
-// ];
-
-// const Sidebar: FC = () => {
-//     const [collapsed, setCollapsed] = useState(false);
-//     const [key, setKey] = useState<string>('booking')
-//     const navigate = useNavigate()
-
-//     return (
-//         <div style={{ padding: "20px 20px" }}>
-
-//             <Menu
-//                 defaultSelectedKeys={['key']}
-//                 defaultOpenKeys={['key']}
-//                 mode="inline"
-//                 theme="dark"
-//                 inlineCollapsed={collapsed}
-//                 onClick={(e) => {
-//                     setKey(e.key)
-//                     navigate(`/${e.key}`)
-//                 }}
-//                 onChange={(e) => {
-//                     e.persist()
-//                 }}
-                
-//                 items={[
-                
-//                     {
-
-//                         key: 'booking',
-//                         icon: <CalendarFilled />,
-//                         label: "Бронь",
-
-//                     },
-//                     {
-//                         key: 'room',
-//                         icon: <CalendarFilled />,
-//                         label: "Номера"
-//                     },
-//                     {
-//                         key: "hotel",
-//                         icon: <CalendarFilled />,
-//                         label: "Отель"
-//                     },
-//                     {
-//                         key: "account",
-//                         icon: <UserOutlined />,
-//                         label: "Аккаунт"
-//                     },
-
-//                     { type: "divider" },
-
-//                     {
-//                         key: 'logout',
-//                         icon: <LogoutOutlined />,
-//                         label: "Выйти"
-//                     },
-                
-//                 ]}
-              
-              
-//             />
-//              <>
-
-//     </>
-//         </div>
-        
-//     );
-// };
-
-// export default Sidebar;
-
-
